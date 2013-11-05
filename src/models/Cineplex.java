1efa69b858929a3cad37efc7d5ef93d8c6587a51
Zhou Xinzi <imdreamrunner@gmail.com>
2013-11-05 18:47:16 +0800

    CZ2002 project

---
+++ src/models/Cineplex.java
@@ -0,0 +1,19 @@
+package models;
+
+import utils.Model;
+
+public class Cineplex extends Model {
+
+	@Override
+	public void loadData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

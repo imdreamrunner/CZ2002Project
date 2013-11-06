440378546a0bf53e065505c5179829f8eaf6d59c
li0031an <li0031an@e.ntu.edu.sg>
2013-11-06 21:32:46 +0800

    bookingCon

--- src/controllers/BookingController.java
+++ src/controllers/BookingController.java
@@ -1,9 +1,24 @@
 package controllers;
 
+import controllers.SystemController;
 import models.Customer;
+import models.Seat;
+import models.Show;
+import models.Movie;
+
+import java.util.Scanner;
 
 public class BookingController {
-	public static int createBooking(int showId, int seatId, Customer customer){
-		return 0;
-	}
+	 public static boolean createBooking(int showId, int seatId, Customer customer){
+	        Show show = Show.getOne(showId);
+	    	Seat seat = Seat.getOne(seatId);
+	    	if (seat.getStatus()==false){
+	    		seat.setName(customer.getName());
+	    		seat.setStatus(true);
+	        	seat.saveData();
+	        	return true;
+	    	}else
+	    		return false;
+	 }
+
 }
--- src/views/BookingInterface.java
+++ src/views/BookingInterface.java
@@ -11,7 +11,8 @@ public class BookingInterface {
     public static void main() {
     	displayMenu();
         Scanner scanner = new Scanner(System.in);
-        int showId = 0, seatId = 0, result=-1;
+        int showId = 0, seatId = 0;
+        boolean result = false;
         Customer customer= null;
         System.out.println("Please enter the show id for booking: ");
         showId = scanner.nextInt();
@@ -70,8 +71,8 @@ public class BookingInterface {
     	customer.setEmail(email);
     	customer.saveData();
     }
-    public static void displayBookingResult(int result){
-    	if (result>=0)
+    public static void displayBookingResult(boolean result){
+    	if (result)
     		System.out.println("***Booking successful!***");
     	else
     		System.out.println("***Booking failure!***");

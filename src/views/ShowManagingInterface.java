69bbac2c0cfe5738890bbd18de8eb3756b238966
Zhou Xinzi <imdreamrunner@gmail.com>
2013-11-06 22:38:58 +0800

    Add someting in show managing

--- src/views/ShowManagingInterface.java
+++ src/views/ShowManagingInterface.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 
 import controllers.ListingController;
 import controllers.SystemController;
+import models.Cinema;
 import models.Cineplex;
 import models.Movie;
+import models.Show;
 
 public class ShowManagingInterface {
+	public static Scanner scanner;
 	public static void main(String[] args) {
 		main();
 	}
 	public static void main() {
-		Scanner scanner = new Scanner(System.in);
+		scanner = new Scanner(System.in);
         System.out.println("1. add a show 2. delete a show 3. list coming shows");
         int choice = scanner.nextInt();
         switch(choice) {
@@ -27,6 +30,15 @@ public class ShowManagingInterface {
 	}
 	public static void addShow() {
 		listMovie();
+		int movieId = scanner.nextInt();
+		Movie movie = Movie.getOne(movieId);
+		listCineplex();
+		int cineplexId = scanner.nextInt();
+		listCinema(cineplexId);
+		int cinemaId = scanner.nextInt();
+		Cinema cinema = Cinema.getOne(cinemaId);
+		System.out.print("Input Month (1 ~ 12): ");
+		int month = scanner.nextInt();
 	}
 	public static void deleteShow() {
 		
@@ -35,6 +47,18 @@ public class ShowManagingInterface {
 		List<Movie> movieList = ListingController.getMovieList();
 		for (Movie movie : movieList) {
 			System.out.println("" + movie.getId() + " " + movie.getName());
+		}
+	}
+	public static void listCineplex() {
+		List<Cineplex> cineplexList = ListingController.getCineplexList();
+		for (Cineplex cineplex : cineplexList) {
+			System.out.println("" + cineplex.getId() + " " + cineplex.getName());
+		}
+	}
+	public static void listCinema(int cineplexId) {
+		List<Cinema> cinemaList = ListingController.getCinemaByCineplex(cineplexId);
+		for (Cinema cinema : cinemaList) {
+			System.out.println("" + cinema.getId() + " " + cinema.getCinemaCode());
 		}
 	}
 }

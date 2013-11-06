61182f5012ccbfcc2d3431ffceb0e336e33c02ed
Zhou Xinzi <imdreamrunner@gmail.com>
2013-11-06 20:17:55 +0800

    Add holiday

--- src/models/Holiday.java
+++ src/models/Holiday.java
@@ -1,5 +1,44 @@
 package models;
 
-public class Holiday {
+import java.util.List;
 
+import utils.Database;
+import utils.Model;
+
+public class Holiday extends Model {
+	private int month;
+	private int day;
+	public void loadData() {
+		month = get("month").getInteger();
+		day = get("day").getInteger();
+	}
+	public void saveData() {
+		set("month", month);
+		set("day", day);
+	}
+	public int getMonth() {
+		return month;
+	}
+	public int getDay() {
+		return day;
+	}
+	public void setMonth(int theMonth) {
+		month = theMonth;
+	}
+	public void setDay(int theDay) {
+		day = theDay;
+	}
+	@SuppressWarnings("unchecked")
+	public static List<Holiday> getAll() {
+		return Database.getAll(Holiday.class);
+	}
+	public static Holiday getOne(int id) {
+		List<Holiday> holidays = getAll();
+		for (Holiday holiday : holidays) {
+			if (holiday.getId() == id) {
+				return holiday;
+			}
+		}
+		return null;
+	}
 }

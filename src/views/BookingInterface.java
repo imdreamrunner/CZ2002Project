0c40b637ed68cf47bda182a313716f57f0b7c5d7
li0031an <li0031an@e.ntu.edu.sg>
2013-11-06 18:54:30 +0800

    booking halfway

--- src/views/BookingInterface.java
+++ src/views/BookingInterface.java
@@ -2,7 +2,38 @@ package views;
 
 public class BookingInterface {
 
+	/*
     public static void main() {
-
+    	displayMenu();
+        Scanner scanner = new Scanner(System.in);
+        int id;
+        choice = scanner.nextInt();
+        System.out.println("Please enter the movie id for book: ");
+        id = scanner.nextInt();
+        System.out.println("Please enter the movie id for book: ");
+        id = scanner.nextInt();
+        
+        while (choice < 3) {
+            switch (choice) {
+                case 1:
+                    BookingInterface.main();
+                    break;
+                case 2:
+                    HistoryInterface.main();
+                    break;
+                case 3:
+                    return;
+                case 4:
+                    //how to quit directly?
+            }
+            displayMenu();
+            choice = scanner.nextInt();
+        }
     }
+
+    public static void displayMenu() {
+        System.out.println("***** Customer Page *****");
+        System.out.printf("Welcome to XXX CinemaComplex Website ");
+    }
+    */
 }

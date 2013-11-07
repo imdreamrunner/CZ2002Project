7906c436fdd95d24a917441826706077515eaf00
Zhou Xinzi <imdreamrunner@gmail.com>
2013-11-07 20:35:27 +0800

    Edit MainController

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -4,15 +4,25 @@ import utils.Controller;
 
 public class MainController extends Controller {
 	public void run() {
-		gi.display("***** Welcome To MOBLIMA *****");
-		String[] menu = {
-				"Please select your entry:",
-                "1. Customer Entry",
-                "2. Staff Entry",
-                "3. Quit",
-                "Please input your choice: "
-		};
-		gi.display(menu);
+		int choice = 0;
+		while (choice < 3) {
+			gi.display("***** Welcome To MOBLIMA *****");
+			String[] menu = {
+					"Please select your entry:",
+	                "1. Customer Entry",
+	                "2. Staff Entry",
+	                "3. Quit",
+	                "Please input your choice: "
+			};
+			gi.display(menu);
+			choice = gi.inputInteger("choice", 1, 3);
+			switch (choice) {
+			case 1:
+				break;
+			case 2:
+				break;
+			}
+		}
 		gi.display("Thank you for using MOBLIMA");
 	}
 }

1ae2ac5c1947e51ae6200d2a8ea1f197aa4f2feb
WangQilin <wangqilin129@gmail.com>
2013-11-08 09:39:10 +0800

    update cusomer controller

--- src/controllers/CustomerController.java
+++ src/controllers/CustomerController.java
@@ -1,42 +1,35 @@
 package controllers;
 
-import java.util.Scanner;
-
 import utils.Controller;
 
 public class CustomerController extends Controller {
 
 	@Override
 	public void run() {
-        displayMenu();
-        Scanner scanner = new Scanner(System.in);
-        int choice;
-        choice = scanner.nextInt();
-        while (choice < 3) {
-            switch (choice) {
-                case 1:
-                    (new BookingController()).run();
-                    break;
-                case 2:
-                    (new HistoryController()).run();
-                    break;
-                case 3:
-                    return;
-                case 4:
-                    //how to quit directly?
+		int choice = 0;
+		while (choice < 4) {
+			gi.display("***** Customer Page *****");
+			String[] menu = {
+					"1. Make a booking",
+					"2. View booking history",
+					"3. Go back to previous page",
+					"4. Quit",
+					"Please input your choice: "
+			};
+		gi.display(menu);
+		choice = gi.inputInteger("choice", 1, 4);
+
+        switch (choice) {
+        	case 1:
+        		(new BookingController()).run();
+        		break;
+        	case 2:
+        		(new HistoryController()).run();
+        		break;
+        	case 3:
+        		return;
             }
-            displayMenu();
-            choice = scanner.nextInt();
         }
+		gi.display("Thank you for using MOBLIMA");
 	}
-
-    public void displayMenu() {
-        System.out.println("***** Customer Page *****");
-        System.out.printf("1. Make a booking \n" +
-                "2. View booking history \n" +
-                "3. Go back to previous page \n" +
-                "4. Quit \n" +
-                "Please input your choice: ");
-    }
-
 }

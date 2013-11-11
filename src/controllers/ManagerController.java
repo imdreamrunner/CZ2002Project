d3fbe2910ab43942b9052a4278899ff2dc1e3c84
Zhou Xinzi <imdreamrunner@gmail.com>
2013-11-11 17:17:10 +0800

    change menu order

--- src/controllers/ManagerController.java
+++ src/controllers/ManagerController.java
@@ -32,19 +32,18 @@ public class ManagerController extends Controller {
         while (choice<=6) {
             switch (choice) {
             case 1:
-            case 2:
             	(new MovieManagingController()).run();
             	break;
-            case 3:
+            case 2:
             	(new ShowManagingController()).run();
                 break;
-            case 4: //set price
+            case 3: //set price
             	(new PriceManagingController()).run();
                 break;
-            case 5: //set holiday
+            case 4: //set holiday
             	(new HolidayManagingController()).run();
                 break;
-            case 6: //generate sales report
+            case 5: //generate sales report
             	(new ReportController()).run();
                 break;
             }
@@ -56,11 +55,11 @@ public class ManagerController extends Controller {
     public void displayMenu() {
         gi.display(new String[] {
                 "1. Update movie list",
-        		"3. Update show list",
-                "4. Set price",
-                "5. Set holiday",
-                "6. Generate sale revenue report",
-                "7. Go back to previous page"});
+        		"2. Update show list",
+                "3. Set price",
+                "4. Set holiday",
+                "5. Generate sale revenue report",
+                "6. Go back to previous page"});
     }
     
     public boolean login() {
